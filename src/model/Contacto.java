30d6ed26576bfe12585a24d529a517ab66ac3840
Martin Cruz <martincruz.z.022@gmail.com>
2026-09-07 13:18:33 -0600

    actualizar clases Contacto y Persona

--- src/model/Contacto.java
+++ src/model/Contacto.java
@@ -1,4 +1,28 @@
 package model;
 
-public class Contacto {
+public class Contacto extends Persona {
+
+    private String telefono;
+
+    public Contacto() {
+        super();
+    }
+
+    public Contacto(String nombre, String apellido, String telefono) {
+        super(nombre, apellido);
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return getNombre() + " " + getApellido() + " - Tel: " + telefono;
+    }
 }
